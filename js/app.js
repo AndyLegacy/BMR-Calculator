68fb6228deb9c15207bd1a8007d56b08d985d759
AndyLegacy <andystav91@googlemail.com>
2017-11-24 10:37:43 +0000

    starting logic

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
-// function calsPerDays() {
-//   function find(id) { return document.getElementById(id)}
-//
-//   let age
-//
-//   }
-//
-//
-//
-// };
+function calsPerDays() {
+  function find(id) { return document.getElementById(id)}
+
+  let age = find('age').value
+  let height = find('height').value * 2.54
+  let weight = find('weight').value / 2.2
+  let result = 0
+  if (find('male').checked)
+    result = 66.47 +  (13.75 * weight) + (5.0 * height - (6.75 * age))
+    
+
+
+  }
+
+
+
+};
